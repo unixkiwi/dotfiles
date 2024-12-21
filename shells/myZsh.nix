c027187ed7d0f836f2fa2af09eaa9b8b4e13d89a
unixkiwi <unixkiwi77@outlook.com>
2024-12-21 17:40:10 +0100

    Initial commit

--- shells/myZsh.nix
+++ shells/myZsh.nix
@@ -14,20 +14,6 @@
         "fast-syntax-highlighting"
         "zsh-autocomplete"
       ];
-
-      customThemes = {
-        powerlevel10k = {
-          name = "powerlevel10k";
-          src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/";
-          file = "powerlevel10k.zsh-theme";
-        };
-      };
-      theme = "powerlevel10k";
     };
-
-    initExtra = ''
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-  '';
-
   };
 }
