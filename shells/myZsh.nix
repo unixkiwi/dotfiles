444633428ae5be4364d10e1a2881934468eed105
unixkiwi <unixkiwi77@outlook.com>
2024-12-22 17:02:48 +0100

    Initial commit

--- shells/myZsh.nix
+++ shells/myZsh.nix
@@ -18,10 +18,38 @@
       searchDownKey = ["\\eOB"];
     };
 
-    initExtra = ''
-      setopt NO_CASE_GLOB
-      zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=*      r:|=*' 'l:|=* r:|=*'
-    '';
+    plugins = [
+      {
+        name = "zsh-autocomplete"; # completes history, commands, etc.
+        src = pkgs.fetchFromGitHub {
+          owner = "marlonrichert";
+          repo = "zsh-autocomplete";
+          rev = "762afacbf227ecd173e899d10a28a478b4c84a3f";
+          sha256 = "1357hygrjwj5vd4cjdvxzrx967f1d2dbqm2rskbz5z1q6jri1hm3";
+        }; # e.g., nix-prefetch-url --unpack https://github.com/marlonrichert/zsh-autocomplete/archive/762afacbf227ecd173e899d10a28a478b4c84a3f.tar.gz
+      }
+    ];
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "z" ];
+      extraConfig = ''
+                # Required for autocomplete with box: https://unix.stackexchange.com/a/778868
+                zstyle ':completion:*' completer _expand _complete _ignored _approximate _expand_alias
+                zstyle ':autocomplete:*' default-context curcontext 
+                zstyle ':autocomplete:*' min-input 0
+
+                setopt HIST_FIND_NO_DUPS
+
+                autoload -Uz compinit
+                compinit
+
+                setopt autocd  # cd without writing 'cd'
+                setopt globdots # show dotfiles in autocomplete list
+      '';
+    };
+
+        
 
     #oh-my-zsh = {
     #  enable = true;
