33b8c97a9c480db4f8ec709438af3c7eb748e1b7
unixkiwi <unixkiwi77@outlook.com>
2024-12-22 16:42:33 +0100

    Initial commit

--- shells/myZsh.nix
+++ shells/myZsh.nix
@@ -13,6 +13,26 @@
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
 
+    historySubstringSearch = {
+      enable = true;
+      searchUpKey = ["\\eOA"];
+      searchDownKey = ["\\eOB"];
+    };
+
+    plugins = [
+        { name = "zsh-users/zsh-autosuggestions"; }
+        { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; }
+        { name =  "ohmyzsh/ohmyzsh oh-my-zsh"; }
+        { name =  "ajeetdsouza/zoxide"; }
+        { name =  "marlonrichert/zsh-autocomplete"; }
+        { name =  "marlonrichert/zsh-edit"; }
+        { name =  "zsh-users/zsh-autosuggestions"; }
+        { name =  "ptavares/zsh-exa"; }
+        { name =  "zsh-users/zsh-syntax-highlighting"; }
+        { name =  "mbenford/zsh-tmux-auto-title"; }
+        { name =  "zsh-users/zsh-history-substring-search"; }
+    ];
+
     #oh-my-zsh = {
     #  enable = true;
     #  plugins = [
