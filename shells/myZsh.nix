af9cf571ab1ad8c4db1a99f1a0c22b9af5aa04aa
unixkiwi <unixkiwi77@outlook.com>
2024-12-22 17:08:50 +0100

    Initial commit

--- shells/myZsh.nix
+++ shells/myZsh.nix
@@ -31,17 +31,17 @@
       plugins = [ "z" ];
       extraConfig = ''
                 # Required for autocomplete with box: https://unix.stackexchange.com/a/778868
-                zstyle ':completion:*' completer _expand _complete _ignored _approximate _expand_alias
-                zstyle ':autocomplete:*' default-context curcontext 
-                zstyle ':autocomplete:*' min-input 0
+                zstyle ':completion:*' # completer _expand _complete _ignored _approximate _expand_alias
+                # zstyle ':autocomplete:*' default-context curcontext 
+                # zstyle ':autocomplete:*' min-input 0
 
                 setopt HIST_FIND_NO_DUPS
 
                 autoload -Uz compinit
                 compinit
 
-                setopt autocd  # cd without writing 'cd'
-                setopt globdots # show dotfiles in autocomplete list
+                setopt autocd
+                setopt globdots
       '';
     };
 
