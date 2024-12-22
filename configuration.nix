788f0afd0f570c7d01d0a90f6ee42dafa9413d44
unixkiwi <unixkiwi77@outlook.com>
2024-12-22 18:53:38 +0100

    Initial commit

--- configuration.nix
+++ configuration.nix
@@ -34,6 +34,9 @@
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
+
+  services.displayManager.defaultSession = "plasma";
+  services.displayManager.sddm.wayland.enable = true;
   
   programs.dconf.enable = true;
 
