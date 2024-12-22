91436c00eb0767e6614b91efba765f3c8e745d3e
unixkiwi <unixkiwi77@outlook.com>
2024-12-22 18:51:58 +0100

    Initial commit

--- configuration.nix
+++ configuration.nix
@@ -32,9 +32,8 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
   
   programs.dconf.enable = true;
 
