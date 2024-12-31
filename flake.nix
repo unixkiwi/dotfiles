f4d5f06c3b5047841c04ed70a806bf9744ac9ff3
unixkiwi <unixkiwi77@outlook.com>
2024-12-31 16:47:13 +0100

    intial commit

--- flake.nix
+++ flake.nix
@@ -25,6 +25,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.kiwi = import ./home.nix;
+            home-manager.backupFileExtension = "backup";
           }
           stylix.nixosModules.stylix
         ];
