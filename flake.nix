0900f6cc41235b0403686f444a3d5e08d2a762bd
unixkiwi <unixkiwi77@outlook.com>
2024-12-31 18:43:43 +0100

    initial commit

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
 
-            home-manager.backupFileExtension = "hm_backup";
+            home-manager.backupFileExtension = null;
                       
             home-manager.users.kiwi = import ./home.nix;
           }
