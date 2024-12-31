80da51176e7cd7785cc4ac59f896134178d1a38d
unixkiwi <unixkiwi77@outlook.com>
2024-12-31 17:07:22 +0100

    changed stylix theme

--- configuration.nix
+++ configuration.nix
@@ -78,7 +78,7 @@
   
     #polarity = "dark";
   
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/everforest.yaml";
   };
 
   # User 'kiwi'
