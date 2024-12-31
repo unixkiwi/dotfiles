d1c2322d4ba1b66d8f6b142f17f8be613eebd590
unixkiwi <unixkiwi77@outlook.com>
2024-12-31 16:55:48 +0100

    intial commit

--- flake.nix
+++ flake.nix
@@ -21,7 +21,8 @@
         inherit system;
         modules = [ 
           ./configuration.nix
-          home-manager.nixosModules.home-manager {
+          home-manager.nixosModules.home-manager 
+          {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.kiwi = import ./home.nix;
