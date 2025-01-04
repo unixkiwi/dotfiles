f71216347cfec4f746567df8fc9b67610c5649dc
unixkiwi <unixkiwi77@outlook.com>
2025-01-04 18:09:24 +0100

    qt stuff

---
+++ modules/qt/default.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, ... }:
+
+{
+  qt = {
+    enable = true;
+
+    platformTheme = "gtk";
+
+    style = {
+      name = "adwaita-dark";
+      package = pkgs.adwaita-qt;
+    };
+  };
+}
