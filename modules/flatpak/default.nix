927e25b06a74cd4ead736a2a73ef26ab6ce44c52
unixkiwi <unixkiwi77@outlook.com>
2025-01-28 17:25:47 +0100

    flatpak stuff

--- modules/flatpak/default.nix
+++ modules/flatpak/default.nix
@@ -1,29 +1,39 @@
 { config, pkgs, ... }:
 
 let
-  flats = [
+  # We point directly to 'gnugrep' instead of 'grep'
+  grep = pkgs.gnugrep;
+
+  # 1. Declare the Flatpaks you *want* on your system
+  desiredFlatpaks = [
     "net.sf.VICE"
   ];
 in
 {
   system.activationScripts.flatpakManagement = {
     text = ''
-      ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+      # 2. Ensure the Flathub repo is added
+      ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists flathub \
+        https://flathub.org/repo/flathub.flatpakrepo
 
-      installedFlats=$(${pkgs.flatpak}/bin/flatpak list --app --columns=application)
-      
-      for installed in $installedFlats; do
-        if ! echo ${toString flats} | ${pkgs.gnugrep}/bin/grep -q $installed; then
-          echo "Removing $installed bc it's not in the wanted flats"
+      # 3. Get currently installed Flatpaks
+      installedFlatpaks=$(${pkgs.flatpak}/bin/flatpak list --app --columns=application)
+
+      # 4. Remove any Flatpaks that are NOT in the desired list
+      for installed in $installedFlatpaks; do
+        if ! echo ${toString desiredFlatpaks} | ${grep}/bin/grep -q $installed; then
+          echo "Removing $installed because it's not in the desiredFlatpaks list."
           ${pkgs.flatpak}/bin/flatpak uninstall -y --noninteractive $installed
         fi
       done
 
-      for app in ${toString flats}; do
-        echo "Installing flat $app"
+      # 5. Install or re-install the Flatpaks you DO want
+      for app in ${toString desiredFlatpaks}; do
+        echo "Ensuring $app is installed."
         ${pkgs.flatpak}/bin/flatpak install -y flathub $app
       done
 
+      # 6. Update all installed Flatpaks
       ${pkgs.flatpak}/bin/flatpak update -y
     '';
   };
