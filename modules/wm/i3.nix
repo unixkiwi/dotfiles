206a308bfca2516a36a1a52329553f4aeb253eee
unixkiwi <unixkiwi77@outlook.com>
2025-01-28 15:48:17 +0100

    initial

--- modules/wm/i3.nix
+++ modules/wm/i3.nix
@@ -15,6 +15,10 @@
       };
       
       defaultWorkspace = "workspace number 1";
+
+      startup = [
+        { command = "xrandr --output Virtual-1 --mode 1920x1080"; always = true }
+      ];
     };
   };
 }
