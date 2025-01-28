ef78c5da515b40498b347db7d3da1815e32ec540
unixkiwi <unixkiwi77@outlook.com>
2025-01-28 17:36:24 +0100

    flatpak stuff

--- configuration.nix
+++ configuration.nix
@@ -190,33 +190,32 @@
   system.activationScripts.flatpakManagement = {
     text = let
         grep = pkgs.gnugrep;
-        desiredFlatpaks = [
+        flats = [
           "net.sf.VICE"
         ];
       in ''
-      # 2. Ensure the Flathub repo is added
+      
       ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists flathub \
         https://flathub.org/repo/flathub.flatpakrepo
 
-      # 3. Get currently installed Flatpaks
-      installedFlatpaks=$(${pkgs.flatpak}/bin/flatpak list --app --columns=application)
+      installedFlats=$(${pkgs.flatpak}/bin/flatpak list --app --columns=application)
 
-      # 4. Remove any Flatpaks that are NOT in the desired list
       for installed in $installedFlatpaks; do
-        if ! echo ${toString desiredFlatpaks} | ${grep}/bin/grep -q $installed; then
-          echo "Removing $installed because it's not in the desiredFlatpaks list."
+        if ! echo ${toString flats} | ${grep}/bin/grep -q $installed; then
+          echo "[FlatpakAutoLOG] Removing $installed"
           ${pkgs.flatpak}/bin/flatpak uninstall -y --noninteractive $installed
         fi
       done
-
-      # 5. Install or re-install the Flatpaks you DO want
-      for app in ${toString desiredFlatpaks}; do
-        echo "Ensuring $app is installed."
+      
+      for app in ${toString flats}; do
+        echo "[FlatpakAutoLOG] Installing $app..."
         ${pkgs.flatpak}/bin/flatpak install -y flathub $app
       done
 
-      # 6. Update all installed Flatpaks
+      echo "[FlatpakAutoLOG] Updating flats..."
       ${pkgs.flatpak}/bin/flatpak update -y
+
+      echo "[FlatpakAutoLOG] Finished FlatpakAutoStartScript!"
     '';
   };
 
